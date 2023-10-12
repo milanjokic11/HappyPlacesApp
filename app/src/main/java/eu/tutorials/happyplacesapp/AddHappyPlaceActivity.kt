c369f7a46078ed7522370052c0753a6cd624eafa
milanjokic11 <123904845+milanjokic11@users.noreply.github.com>
2023-10-12 18:42:07 -0500

    updating UI and adding permissions for READ/WRITE

--- app/src/main/java/eu/tutorials/happyplacesapp/AddHappyPlaceActivity.kt
+++ app/src/main/java/eu/tutorials/happyplacesapp/AddHappyPlaceActivity.kt
@@ -1,11 +1,122 @@
 package eu.tutorials.happyplacesapp
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var addPlaceToolbar: Toolbar? = null
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+    private var etDate: EditText? = null
+    private var tvAddImage: TextView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
+        addPlaceToolbar = findViewById(R.id.add_place_toolbar)
+
+        setSupportActionBar(addPlaceToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        addPlaceToolbar!!.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener{
+            view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateView()
+        }
+        etDate = findViewById<EditText>(R.id.et_date)
+        etDate?.setOnClickListener(this)
+        tvAddImage = findViewById<TextView>(R.id.tv_add_image)
+        tvAddImage?.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(this@AddHappyPlaceActivity, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+            R.id.tv_add_image -> {
+                val picDialog = AlertDialog.Builder(this)
+                picDialog.setTitle("Select Action")
+                val picDialogItems = arrayOf("Select photo from Gallery", "Capture photo from Camera")
+                picDialog.setItems(picDialogItems) {
+                    _, which ->
+                    when (which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(this@AddHappyPlaceActivity, "Camera selection coming soon...", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                picDialog.show()
+            }
+        }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object: MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()) {
+                        Toast.makeText(this@AddHappyPlaceActivity, "Storage READ/WRITE permission are granted...", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>,token: PermissionToken ) {
+                    showRationaleDialogForPermissions()
+                }
+            }).onSameThread().check()
+    }
+
+    private fun showRationaleDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage("It seems you have turned off the permissions required for this feature... It can be enabled under Application Settings...")
+            .setPositiveButton("GO TO SETTINGS") {
+                _, _ ->
+                    try {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        startActivity(intent)
+                    } catch (e: ActivityNotFoundException) {
+                        e.printStackTrace()
+                    }
+            }.setNegativeButton("Cancel") {
+                dialog, which ->
+                    dialog.dismiss()
+            }.show()
+    }
+
+    private fun updateDateView() {
+        val format = "MM.dd.yyyy"
+        val sdf = SimpleDateFormat(format, Locale.getDefault())
+        etDate?.setText(sdf.format(cal.time).toString())
     }
 }
